eb13afbe0b903d0703263187fd64314ca4605b1b
Ririena <nerllysurina@gmail.com>
2024-06-19 05:33:51 +0700

    -dev

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2bupoqh7teo"
+    "revision": "0.b3e2bm5d9to"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
